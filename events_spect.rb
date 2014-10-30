fee0c4e8746961936175ed4a0b8e7cd32af9ea8a
Mat Holroyd <contact@matholroyd.com>
2014-10-31 09:18:22 +1100

    Tested counts groups 1 level deep all good
    - Left unspecified what should happen if event doesn’t match any key

--- events_spect.rb
+++ events_spect.rb
@@ -1,14 +1,6 @@
 require 'rspec'
 require './events'
 
-events = [
-  { event: 'bill.split', user: 'Ray Green', city: 'Boston', time_of_day: 'morning', timestamp: 23981398 },
-  { event: 'promo.used', user: 'Jon Wicks', city: 'New York', time_of_day: 'afternoon', amount: 10.0, timestamp: 93219323 },
-  { event: 'promo.used', user: 'Robin Chou', city: 'New York', time_of_day: 'afternoon', amount: 15.0, timestamp: 28138233 },
-  { event: 'bill.split', user: 'John Malcom', city: 'Chicago', time_of_day: 'evening', timestamp: 32189389 },
-  { event: 'bill.split', user: 'Mark Wang', city: 'Boston', time_of_day: 'evening', timestamp: 43890121 }
-]
-
 describe Events do
   context "group_and_count" do
     
@@ -16,6 +8,31 @@ describe Events do
       it "should count 1 if just 1 unique item, 1 level deep" do 
         Events.new([{event: 'a'}]).group_and_count(['event']).should == {"a" => 1}
       end
+      
+      it "should count different groups properly, 1 level deep" do
+        Events.new([
+          {event: 'a'},
+          {event: 'a'},
+          {event: 'b'}
+        ]).group_and_count(['event']).should == {
+          "a" => 2,
+          "b" => 1
+        }
+      end
+      
+      # Hmmm...actually this test case is specified. Leave for now
+      # it "should not include items that do not match the group" do
+      #
+      #   Events.new([
+      #     {event: 'a'},
+      #     {something_else: 'blah'}
+      #   ]).group_and_count(['event']).should == {
+      #     "a" => 1
+      #   }
+      # end
+
+      
+      
     end
     
   end
