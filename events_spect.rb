a8685ab5d9cb0135b3b8ec9fead7eed5e895de47
Mat Holroyd <contact@matholroyd.com>
2014-10-31 10:00:31 +1100

    Added the actual test verbatim to specs. Passes

--- events_spect.rb
+++ events_spect.rb
@@ -62,6 +62,33 @@ describe Events do
         }
       end
       
+      it "should pass the specified test" do
+        events = [
+          { event: 'bill.split', user: 'Ray Green', city: 'Boston', time_of_day: 'morning', timestamp: 23981398 },
+          { event: 'promo.used', user: 'Jon Wicks', city: 'New York', time_of_day: 'afternoon', amount: 10.0, timestamp: 93219323 },
+          { event: 'promo.used', user: 'Robin Chou', city: 'New York', time_of_day: 'afternoon', amount: 15.0, timestamp: 28138233 },
+          { event: 'bill.split', user: 'John Malcom', city: 'Chicago', time_of_day: 'evening', timestamp: 32189389 },
+          { event: 'bill.split', user: 'Mark Wang', city: 'Boston', time_of_day: 'evening', timestamp: 43890121 }
+        ]
+        
+        Events.new(events).group_and_count(['event','city','time_of_day']).should == {
+          "bill.split" => {
+             "Boston" => {
+               "morning" => 1,
+               "evening" => 1
+             },
+             "Chicago" =>  {
+               "evening" => 1
+             }
+           },
+           "promo.used" => {
+             "New York" => {
+               "afternoon" => 2
+             }
+           }
+        }
+        
+      end
       
     end
     
